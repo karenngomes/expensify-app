af1ad47c95467224206003c8a6f2186d08fcb989
Karen Gomes <kngs@ic.ufal.br>
2018-11-02 01:34:18 -0300

    organizing routes with react-router

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ExpenseDashboardPage = () => <div>This is my dashboard component</div>;
+
+export default ExpenseDashboardPage;
---
+++ src/components/HelpPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const HelpPage = () => <div>This is my help expense component</div>;
+
+export default HelpPage;
