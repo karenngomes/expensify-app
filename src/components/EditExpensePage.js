969e5cbbf1f95376e25553df49b72c0e29ee523f
Karen Gomes <kngs@ic.ufal.br>
2018-11-02 01:57:44 -0300

    query strings and url parameters

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const EditExpensePage = props => {
   console.log(props);
-  return <div>This is my edit expense component</div>;
+  return <div>Editing the expense with id of {props.match.params.id}</div>;
 };
 
 export default EditExpensePage;
--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,6 @@ const Header = () => (
     <NavLink to="/create" activeClassName="is-active">
       Create
     </NavLink>
-    <NavLink to="/edit" activeClassName="is-active">
-      Edit
-    </NavLink>
     <NavLink to="/help" activeClassName="is-active">
       Help
     </NavLink>
