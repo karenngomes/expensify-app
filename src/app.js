fdbce483cb309c2f7db7958a900742e11bbe7b20
Karen Gomes <kngs@ic.ufal.br>
2018-11-02 01:15:12 -0300

    setting up budget app

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+ReactDOM.render(<p>Hello, world</p>, document.getElementById("app"));
