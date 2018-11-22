0117774b887105a7d31e49cc1e10a23eb20a4341
Karen Gomes <kngs@ic.ufal.br>
2018-11-22 09:32:09 -0300

    organizing redux in files

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -55,6 +55,7 @@ const setEndDate = endDate => ({
   type: "SET_END_DATE",
   endDate
 });
+
 // Expenses Reducer
 
 const expensesReducerDefaultState = [];
---
+++ src/selectors/expenses.js
@@ -0,0 +1,24 @@
+// Get visible expenses
+
+export default (expenses, { text, sortyBy, startDate, endDate }) => {
+  return expenses
+    .filter(expense => {
+      const startDateMatch =
+        typeof startDate !== "number" || expense.createdAt >= startDate;
+      const endDateMatch =
+        typeof endDate !== "number" || expense.createdAt <= endDate;
+      // convert both strings to lower case
+      // includes
+      const textMatch = expense.description
+        .toLowerCase()
+        .includes(text.toLowerCase());
+
+      return startDateMatch && endDateMatch && textMatch;
+    })
+    .sort((a, b) => {
+      if (sortyBy === "date") {
+        return a.createdAt < b.createdAt ? 1 : -1;
+      }
+      return a.amount < b.amount ? 1 : -1;
+    });
+};
