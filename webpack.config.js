e629997599473c291773e9c30f6228b66cd9e9cc
Karen Gomes <kngs@ic.ufal.br>
2019-01-29 14:36:36 -0300

    connecting store and component with react-redux

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 console.log(path.join(__dirname, "public"));
 
 module.exports = {
-  entry: "./src/playground/hoc.js",
+  entry: "./src/app.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
