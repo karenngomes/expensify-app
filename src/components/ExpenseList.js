ac391614e6c91542791293fbf8c931d531b03167
Karen Gomes <kngs@ic.ufal.br>
2019-03-04 16:53:55 -0300

    controlled inputs for filters

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,22 +3,19 @@ import { connect } from "react-redux";
 import ExpenseListItem from "./ExpenseListItem";
 import selectExpenses from "../selectors/expenses";
 
-const ExpenseList = props => (
+const ExpenseList = ({ expenses }) => (
   <div>
     <h1>Expense List</h1>
     <ul>
-      {props.expenses.map(expense => {
+      {expenses.map(expense => {
         return <ExpenseListItem key={expense.id} {...expense} />;
       })}
     </ul>
   </div>
 );
 
-const mapStateToProps = state => {
-  return {
-    expenses: selectExpenses(state.expenses, state.filters),
-    filters: state.filters
-  };
-};
+const mapStateToProps = state => ({
+  expenses: selectExpenses(state.expenses, state.filters)
+});
 
 export default connect(mapStateToProps)(ExpenseList);
