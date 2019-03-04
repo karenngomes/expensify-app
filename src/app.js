606cbb5b515d45a6cc0eb1b15261006cbb55ebd5
Karen Gomes <kngs@ic.ufal.br>
2019-03-04 17:36:46 -0300

    dropdown for picking sortBy

--- src/app.js
+++ src/app.js
@@ -4,21 +4,17 @@ import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
+import { sortyByAmount } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water bill", amount: 500 }));
-store.dispatch(addExpense({ description: "Gas bill", amount: 20 }));
-store.dispatch(setTextFilter("water"));
-
-setTimeout(() => {
-  store.dispatch(setTextFilter("bill"));
-}, 3000);
-
+store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
+store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
+store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
+store.dispatch(sortyByAmount());
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
-import { setTextFilter } from "../actions/filters";
+import { setTextFilter, sortyByAmount, sortyByDate } from "../actions/filters";
 
-const ExpenseListFilters = props => (
-  <div>
-    <input
-      type="text"
-      value={props.filters.text}
-      onChange={e => {
-        props.dispatch(setTextFilter(e.target.value));
-      }}
-    />
-  </div>
-);
+const ExpenseListFilters = ({ filters, dispatch }) => {
+  const { text, sortyBy } = filters;
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={e => dispatch(setTextFilter(e.target.value))}
+      />
+      <select
+        value={sortyBy}
+        onChange={e =>
+          dispatch(e.target.value === "date" ? sortyByDate() : sortyByAmount())
+        }
+      >
+        <option value="date">Date</option>
+        <option value="amount">Amount</option>
+      </select>
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   filters: state.filters
