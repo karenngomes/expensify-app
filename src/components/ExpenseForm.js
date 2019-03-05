ba233d3b2d6e79ab02dea658914316bbd3359e29
Karen Gomes <kngs@ic.ufal.br>
2019-03-04 21:51:40 -0300

    submit and validation

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -9,11 +9,13 @@ export default class ExpenseForm extends Component {
     note: "",
     amount: "",
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: ""
   };
 
   onDescriptionChange = e => {
     const description = e.target.value;
+
     this.setState({
       description
     });
@@ -21,6 +23,7 @@ export default class ExpenseForm extends Component {
 
   onNoteChange = e => {
     const note = e.target.value;
+
     this.setState({
       note
     });
@@ -28,6 +31,7 @@ export default class ExpenseForm extends Component {
 
   onAmountChange = e => {
     const amount = e.target.value;
+
     if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState({
         amount
@@ -45,17 +49,31 @@ export default class ExpenseForm extends Component {
     this.setState({ calendarFocused: focused });
   };
 
+  onSubmit = e => {
+    e.preventDefault();
+    const { description, amount } = this.state;
+
+    if (!description || !amount) {
+      this.setState({ error: "Please provide description and amount" });
+    } else {
+      this.setState({ error: "" });
+    }
+  };
+
   render() {
     const {
       description,
       note,
       amount,
       createdAt,
-      calendarFocused
+      calendarFocused,
+      error
     } = this.state;
+
     return (
       <div>
-        <form>
+        {error && <p>{error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
