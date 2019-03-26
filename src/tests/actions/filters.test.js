a7846eff73ede93b403eb7d27eaf0041a0f9cc21
Karen Gomes <kngs@ic.ufal.br>
2019-03-26 20:55:54 -0300

    feat(action/filters): add test to check if setted text filter action object with default values

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -1,5 +1,5 @@
 import moment from "moment";
-import { setStartDate, setEndDate } from "../../actions/filters";
+import { setStartDate, setEndDate, setTextFilter } from "../../actions/filters";
 
 test("should generate set start date action object", () => {
   const action = setStartDate(moment(0));
@@ -16,5 +16,14 @@ test("should generate set end date action object", () => {
   expect(action).toEqual({
     type: "SET_END_DATE",
     endDate: moment(0)
+  });
+});
+
+test("should setup set text filter action object with default values", () => {
+  const action = setTextFilter();
+
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text: ""
   });
 });
