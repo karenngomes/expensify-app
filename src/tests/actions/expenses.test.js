29689d209b77f05abbd23ede3600595c83b9ded6
Karen Gomes <kngs@ic.ufal.br>
2019-03-26 20:23:16 -0300

    feat(action/expenses): add test to check add expense action object with provided values

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -21,3 +21,20 @@ test("should setup edit expense action object", () => {
     }
   });
 });
+
+test("should setup add expense action object with provided values", () => {
+  const expenseData = {
+    description: "Rent",
+    amount: 109500,
+    createdAt: 1000,
+    note: "This was last months rent"
+  };
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: "ADD_EXPENSE",
+    expense: {
+      ...expenseData,
+      id: expect.any(String)
+    }
+  });
+});
