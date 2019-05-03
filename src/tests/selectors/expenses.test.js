9ae06ed18192042e77edd7444539dfb3b6bf266a
Karen Gomes <karen.nayara@audora.com.br>
2019-05-03 15:39:28 -0300

    feat(selectors): add test to check filters by endDate

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -52,3 +52,15 @@ test("should filter by startDate", () => {
 
   expect(result).toEqual([expenses[2], expenses[0]]);
 });
+
+test("should filter by endDate", () => {
+  const filters = {
+    text: "",
+    sortBy: "date",
+    startDate: undefined,
+    endDate: moment(0).add(2, "days")
+  };
+  const result = selectExpenses(expenses, filters);
+
+  expect(result).toEqual([expenses[0], expenses[1]]);
+});
