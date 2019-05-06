020be1f6779b62352633365156b77c7167ba90f7
Karen Gomes <karen.nayara@audora.com.br>
2019-05-06 11:40:05 -0300

    feat(components/header): add a test to check if text inside h1 element come correctly

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,12 @@
 import React from "react";
-import ReactShallowRenderer from "react-test-renderer/shallow";
+import { shallow } from "enzyme";
 import Header from "../../components/Header";
 
 test("should render Header correctly", () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper.find("h1").text()).toBe("Expensify");
+  
+  //   const renderer = new ReactShallowRenderer();
+  //   renderer.render(<Header />);
+  //   expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
